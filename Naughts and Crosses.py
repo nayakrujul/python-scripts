94c7ba36e48e723528140834de59dbf1ef0f95e7
First Name Last Name <55329600+nayakrujul@users.noreply.github.com>
2020-01-04 19:40:36 +0000

    Add files via upload

--- Naughts and Crosses.py
+++ Naughts and Crosses.py
@@ -2,102 +2,89 @@ import sys
 import time
 import numpy as np
 
-print("")
-print("Welcome to RN Naughts and Crosses")
-print("")
-userinput = raw_input("How many squares do you want for each side? ")
 
-boardlist = []
+# Function to validate board size (user input)
+def validate_userinput(userinput):
+    if(str.isdigit(str(userinput))):
+        boardsize = int(userinput)
+        if(boardsize > 10 or boardsize <= 2):
+            print("Invalid board size. Must be between 3 and 10. DECLINED!")
+            print("")
+            sys.exit()
+    else:
+        print("Invalid input. Must be a positive integer. DECLINED!")
+        print("")
+        sys.exit()
 
-def initialise_boardlist(userinput):
-    # boardlist = [[0,0,0],[0,0,0],[0,0,0]]
-    # boardlist1 = [0] * int(userinput)
-    # boardlist = boardlist1 * int(userinput)
-    for m in range(int(userinput)):
+
+# Function to initialise the boardlist
+def initialise_boardlist(boardsize):
+    boardlist = []
+    # Populate all values with 0
+    for m in range(boardsize):
         boardlist.insert(m, [])
-        for n in range(int(userinput)):
+        for n in range(boardsize):
             boardlist[m].append(0)
     # print(boardlist)
     return boardlist
 
-boardlist = initialise_boardlist(userinput)
 
-if(str.isdigit(str(userinput))):
-    repeat = int(userinput)
-    if(repeat > 10 or repeat <= 0):
-        print("Invalid board size. DECLINED!")
-        print("")
-        sys.exit()
-else:
-    print("Invalid input. DECLINED!")
+# Function to print empty board
+def print_empty_board(boardsize):
     print("")
-    sys.exit()
+    print("Here is the empty board:")
+    print("")
+    dash = " ---"
+    slash = "|   "
+    dash2 = dash * boardsize
+    slash2 = slash * (boardsize + 1)
 
-print("")
-print("Here is the empty board:")
-print("")
-dash = " ---"
-slash = "|   "
-dash2 = dash * repeat
-slash2 = slash * (repeat + 1)
-
-while(repeat > 0):
+    while(boardsize > 0):
+        print(dash2)
+        print(slash2)
+        boardsize = boardsize - 1
     print(dash2)
-    print(slash2)
-    repeat = repeat - 1
-print(dash2)
+    print("")
 
-# value = 0
-# while(value == 0):
-#     row = raw_input("Enter row: ")
-#     column = raw_input("Enter column: ")
-#     if((str.upper(row) == "A" or str.upper(row) == "B" or str.upper(row) == "C") and (str(column) == "1" or str(column) == "2" or str(column) == "3")):
-#         print("Values accepted.")
-#     else:
-#         print("Values declined.")
-
-print("")
-
-
+# Function to check winner
 def check_winner(boardlist):
     check_row_winner(boardlist)
-    # print("")
+
     check_column_winner(boardlist)
-    # print("")
+
     check_left_diagonal_winner(boardlist)
-    # print("")
+
     check_right_diagonal_winner(boardlist)
+
     print("")
+
     check_board(boardlist)
     print("No winners yet. Please carry on.")
 
 
+# Function to check if winner in any of the rows
 def check_row_winner(boardlist):    
-    # print("Checking board for each row...")   
-    # check rows
     for a in range(int(userinput)):
         row = boardlist[a]
         if(len(set(row)) == 1 and row[0] != 0):
-            print("Player " + str(row[1]) + " has won.")
+            print("Player " + str(row[0]) + " has won.")
             sys.exit()
     # print("No row winners")
 
 
+# Function to check if winner in any of the columns
 def check_column_winner(boardlist):
-    # check columns
-    # print("Checking board for each column...")
     transposedBoardlist = np.array(boardlist).T
     for a in range(int(userinput)):
         row = transposedBoardlist[a]
         if(len(set(row)) == 1 and row[0] != 0):
-            print("Player " + str(row[1]) + " has won.")
+            print("Player " + str(row[0]) + " has won.")
             sys.exit()
     # print("No column winners")
 
 
+# Function to check if winner in the top-left to bottom-right diagonal
 def check_left_diagonal_winner(boardlist):
-    # check left diagonal
-    # print("Checking board for the left diagonal...")
     for x in range(int(userinput)):
         if(x == 0):
             firstValue = boardlist[x][x]
@@ -112,10 +99,10 @@ def check_left_diagonal_winner(boardlist):
             return 0
     print("Player " + str(firstValue) + " has won")
     sys.exit()
-    
+
+
+# Function to check if winner in the top-right to bottom-left diagonal
 def check_right_diagonal_winner(boardlist):
-    # check right diagonal
-    # print("Checking board for the right diagonal...")
     for x in range(int(userinput)):
         if(x == 0):
             firstValue = boardlist[x][(int(userinput) - x - 1)]
@@ -132,6 +119,7 @@ def check_right_diagonal_winner(boardlist):
     sys.exit()
 
 
+# Function to check if any coordinates available for further moves
 def check_board(boardlist):
     validFlag = 0
     for a in range(len(boardlist)):
@@ -144,27 +132,30 @@ def check_board(boardlist):
         sys.exit()
 
 
+# Function to print the current board
 def print_board(boardlist):
     for a in range(len(boardlist)):
         for b in range(len(boardlist[a])):
             print(boardlist[a][b]),
         print("\n")
 
-def initialise_boardlist(userinput, boardlist):
-    boardlist = [[0,0,0],[0,0,0],[0,0,0]]
 
-# boardlist = []
-# boardlistline = []
-# for x in range(0, int(userinput)):
-#   for y in range(0, int(userinput)):
-#     boardlistline.append(0)
-#   boardlist.append(boardlistline)
-#   boardlistline = []
-# print("Printing current board...")
-# time.sleep(1)
-# for z in range(0, int(userinput)):
-#   print(boardlist[z])
-# print("")
+
+# -------------------------------------------------------------------
+# Main Code
+print("")
+print("Welcome to RN Naughts and Crosses")
+print("")
+userinput = raw_input("How many squares do you want for each side? ")
+
+validate_userinput(userinput)
+
+boardsize = int(userinput)
+
+boardlist = []
+boardlist = initialise_boardlist(boardsize)
+
+print_empty_board(boardsize)
 
 player = 1
 
@@ -184,21 +175,21 @@ while(0 == 0):
     
 
     if(len(rowColumnSplit) == 2):
-        # print("Values accepted past first stage.")
+        # print("Values accepted past first stage (Wrong number of values).")
 
         if(str.isdigit(rowColumnSplit[0]) and str.isdigit(rowColumnSplit[1])):
-            # print("Values accepted past second stage.")
-            split1 = rowColumnSplit[0]
-            split2 = rowColumnSplit[1]
+            # print("Values accepted past second stage (Not numeric values).")
+            rowValue = int(rowColumnSplit[0])
+            columnValue = int(rowColumnSplit[1])
 
-            if(int(split1) <= 3 and int(split1) >= 1 and int(split2) <= 3 and int(split2) >= 1):
-                # print("Values accepted past third stage.")
+            if(rowValue <= boardsize and rowValue >= 1 and columnValue <= boardsize and columnValue >= 1):
+                # print("Values accepted past third stage (Coordinates not on board).")
 
-                if(boardlist[(int(split1) - 1)][(int(split2) - 1)] == 0):
-                    # print("Values accepted past fourth stage.")
+                if(boardlist[(rowValue - 1)][(columnValue - 1)] == 0):
+                    # print("Values accepted past fourth stage (Coordinates not available).")
                     print("")
                     # print("Inputting values...")
-                    boardlist[(int(split1) - 1)][(int(split2) - 1)] = player
+                    boardlist[(rowValue - 1)][(columnValue - 1)] = player
                     # print("Values inputted")
                     print("")
 
@@ -209,21 +200,22 @@ while(0 == 0):
                     else:
                         errorExit = raw_input("An error has occured. We are trying to fix it as soon as we can. To exit, press enter.")
                         sys.exit()
+          
                     print_board(boardlist)
                     check_winner(boardlist)
                     print("Continuing...")
                     continue
 
                 else:
-                    print("Values declined (fourth stage)")
+                    print("Values declined (Coordinates not available)")
 
             else:
-                print("Values declined (third stage)")
+                print("Values declined (Coordinates not on board)")
 
         else:
-            print("Values declined (second stage)")
+            print("Values declined (Not numeric values)")
 
     else:
-        print("Values declined (first stage)")
+        print("Values declined (Wrong number of values)")
 
 print("")
